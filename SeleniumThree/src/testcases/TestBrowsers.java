3f24475b768db3d60875962a257ab7cf1b1dff9f
unknown <chandra1511amensys@gmail.com>
2020-09-09 22:11:49 +0530

    f

---
+++ SeleniumThree/src/testcases/TestBrowsers.java
@@ -0,0 +1,26 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;;
+
+public class TestBrowsers 
+{
+	public static void main(String args[])
+	{
+		//Programme for FireFox Driver
+		/*
+		System.setProperty("webdriver.gecko.driver", "D:\\BrowserNatives\\geckodriver.exe");
+		FirefoxDriver fox=new FirefoxDriver();
+		fox.get("http://way2automation.com");
+		*/
+		
+		
+		//Programme for Chrome Driver
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\BrowserNatives\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver chrome=new ChromeDriver();
+		chrome.get("http://way2automation.com");
+ 		
+	}
+
+}
