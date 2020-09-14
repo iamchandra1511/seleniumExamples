fa6f72752ce3ccd0f2cc5d3e8685a1e0cfe812dd
unknown <chandra1511amensys@gmail.com>
2020-09-14 20:34:33 +0530

    New

---
+++ TestNGLearning/src/test/java/customListeners/Listeners.java
@@ -0,0 +1,62 @@
+package customListeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class Listeners implements ITestListener
+{
+
+	public void onTestStart(ITestResult result) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult result) 
+	{
+
+
+		System.out.println("Passed Test - "+result.getName());
+		
+	}
+
+	public void onTestFailure(ITestResult result)
+	{
+
+		System.setProperty("org.uncommons.reportng.escape-output","false");
+		Reporter.log("<a href=\"e:\\screenshot\\error.jpg\" target=\"_blank\">Screenshot link</a>");
+		Reporter.log("<br>");
+		Reporter.log("<a href=\"e:\\screenshot\\error.jpg\" target=\"_blank\"><img height=200 width=200 src=\"e:\\screenshot\\error.jpg\"></a>");
+		
+		
+		System.out.println("Capturing Screenshot for the Failed test -- "+result.getName());
+		
+	}
+
+	public void onTestSkipped(ITestResult result) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext context) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
